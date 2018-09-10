ef57394c226f9ef1e38399a81f36936162af2ded
lucasrodrigues10 <lucasrodrigues10@outlook.com>
2018-09-10 04:20:35 -0300

    adicionei descricao do meta

--- js/registrar.js
+++ js/registrar.js
@@ -1,29 +1,23 @@
-$('#registrar').click(function(){
-	dados = {	
-			nome:$("#regNome").val(),
-			senha:$("#regPass").val(),
-			email:$("#regEmail").val(),
-			cpf:$("#regCpf").val(),
-			rg:$("#regRg").val(),
-			tel:$("#regTel").val(),
-			tipo:$("#regTipo").val().toLowerCase()
-	}
-	
-	
-	$.ajax({
-		url:"http://"+ host+ "/registrar",
-		type: "post",
-		dataType: "json",
-		contentType: 'application/json',
-		data: JSON.stringify(dados),
-		statusCode:{
-			201: function (response){
-				//alerta usuario que conta foi criada
-				console.log("conta criada com sucesso");
-			}
-		}
-		
-	});
-	
-	
+$('#registrar').click(function () {
+    dados = {
+        nome: $("#regNome").val(),
+        senha: $("#regPass").val(),
+        email: $("#regEmail").val()
+    }
+
+    $.ajax({
+        url: "http://" + host + "/registrar",
+        type: "post",
+        dataType: "json",
+        contentType: 'application/json',
+        data: JSON.stringify(dados),
+        statusCode: {
+            201: function (response) {
+                //alerta usuario que conta foi criada
+                console.log("conta criada com sucesso");
+            }
+        }
+
+    });
+
 })
