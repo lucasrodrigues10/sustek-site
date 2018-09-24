fb8455299bb954e1cd3022a6b3b4e9b1367403ee
lucasrodrigues10 <lucasrodrigues10@outlook.com>
2018-09-24 00:06:42 -0300

    login adicionado com sucesso

--- dashboard/js/login.js
+++ dashboard/js/login.js
@@ -3,26 +3,38 @@ $(document).ready(function () {
         usuario = $('#input-usuario').val();
         senha = $('#input-senha').val();
 
-        url = 'http://u643580869.hostingerapp.com/login';
-
         //cria json para login
-        //jsonLogin = JSON.stringify({login: usuario,pass: senha});
-        jsonLogin = {"login": "teste@sql", "pass": "123"};
-        console.log(jsonLogin);
+        jsonLogin = JSON.stringify({login: usuario, pass: senha});
         $.ajax({
             type: "POST",
             url: 'http://u643580869.hostingerapp.com/login',
             data: jsonLogin,
             contentType: "application/json; charset=utf-8",
             dataType: "json",
-            crossDomain: true,
-            success: function (data) {
-                alert("Deu bom");
-                alert(data);
-                window.location.href = "index.html";
+            statusCode: {
+                200: function () {
+                    console.log("200 - Success");
+                    window.location.href = "index.html";
+
+                },
+                401: function (request, status, error) {
+                    alert('Dados Incorretos');
+                    console.log("404 - Not Found");
+                    console.log(error);
+                },
+                404: function (request, status, error) {
+                    console.log("404 - Not Found");
+                    console.log(error);
+                },
+                503: function (request, status, error) {
+                    console.log("503 - Server Problem");
+                    console.log(error);
+                }
             },
-            error: function () {
-                alert("Errouuuuu");
+            error: function (jqXHR, status, error) {
+                console.log(jqXHR);
+                console.log(status);
+                console.log(error);
             }
         });
     });
